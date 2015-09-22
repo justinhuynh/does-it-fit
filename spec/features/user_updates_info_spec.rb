34beeac6f2b02e8c433578c997a4280ad6a711fe
Eric Zell <EricJZell@gmail.com>
2015-09-22 10:29:21 -0400

    fixing hound errors

--- spec/features/user_updates_info_spec.rb
+++ spec/features/user_updates_info_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'user updates info', %Q{
+feature 'user updates info', %{
   As an existing user
   I want to update my account
   So that I can reflect up-to-date information
@@ -15,42 +15,29 @@ feature 'user updates info', %Q{
   scenario 'Successfully update registration information' do
     user = FactoryGirl.create(:user)
     visit new_user_session_path
-
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-
-
     click_button 'Log in'
-
     expect(page).to have_content('Signed in successfully.')
     expect(page).to have_content('Sign Out')
     click_link 'Edit Your User Profile'
-
-
     fill_in 'Email', with: 'john@yahoo.com'
     fill_in 'Password', with: 'Password2'
     fill_in 'Password confirmation', with: 'Password2'
     fill_in 'Current password', with: user.password
     click_button 'Update'
-
     expect(page).to have_content('Your account has been updated successfully.')
   end
 
   scenario 'provide invalid registration information' do
     user = FactoryGirl.create(:user)
     visit new_user_session_path
-
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-
-
     click_button 'Log in'
-
     expect(page).to have_content('Signed in successfully.')
     expect(page).to have_content('Sign Out')
     click_link 'Edit Your User Profile'
-
-
     fill_in 'Email', with: 'johnyahoo.com'
     fill_in 'Password', with: 'Password2'
     fill_in 'Password confirmation', with: 'Password2'
