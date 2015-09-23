dc0871315f151aef127324213586f0b27fd6fe6c
Sorachantha Chan <xwsgbx@gmail.com>
2015-09-23 14:13:37 -0400

    fix hound errors

--- spec/features/user_views_review_of_product_spec.rb
+++ spec/features/user_views_review_of_product_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'pry'
 
-feature 'user views reviews of a product', %Q{
+feature 'user views reviews of a product', %{
   As a prospective user
   I want to views the reviews of a product
   So that I can know what to expect of a product
@@ -13,12 +13,15 @@ feature 'user views reviews of a product', %Q{
   let!(:user) { FactoryGirl.create(:user) }
   let!(:category) { FactoryGirl.create(:category) }
   let!(:brand) { FactoryGirl.create(:brand) }
-  let!(:product) { FactoryGirl.create(:product, category_id: category.id, brand_id: brand.id) }
-  let!(:review) { FactoryGirl.create(:review, user_id: user.id, product_id: product.id)}
+  let!(:product) do
+    FactoryGirl.create(:product, category_id: category.id, brand_id: brand.id)
+  end
+  let!(:review) do
+    FactoryGirl.create(:review, user_id: user.id, product_id: product.id)}
+  end
 
   scenario 'see reviews of a product' do
     visit product_path(product)
-    save_and_open_page
     expect(page).to have_content(product.reviews[0].title)
     expect(page).to have_content(product.reviews[0].body)
   end
