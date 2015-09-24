19f5a4b966feb7c6b8c2995d243d560295090bf2
Sorachantha Chan <xwsgbx@gmail.com>
2015-09-24 13:53:23 -0400

    christina comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,11 +9,17 @@ class ReviewsController < ApplicationController
     if @review.save
       flash[:notice] = 'Review successfully added'
       redirect_to product_path(@product)
+    elsif
+      @review.errors.full_messages.join(' ').include?('User')
+      flash[:error] = 'You must be signed in'
+      redirect_to product_path(@product)
     else
       flash[:error] = @review.errors.full_messages.join(' ')
       redirect_to product_path(@product)
     end
   end
+
+  protected
 
   def review_params
     params.require(:review).permit(:title, :body, :product, :product_fit,
--- spec/features/user_adds_review_of_a_product_spec.rb
+++ spec/features/user_adds_review_of_a_product_spec.rb
@@ -7,33 +7,46 @@ feature 'user adds review of a product', %{
 
   Acceptance Criteria:
   - [√] I must be logged in
-  - [ ] I must provide title, body, product fit
-  - [ ] I must recieve an error message for invalid form
-  - [ ] I must get a success message and be brougt to the home page on success
+  - [√] I must provide title, body, product fit
+  - [√] I must recieve an error message for invalid form
+  - [√] I must get a success message and be brougt to the home page on success
 } do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:product) { FactoryGirl.create(:product) }
 
-  scenario 'user successfully adds review to product' do
-    sign_in(user)
+  context "user is signed in" do
+    before (:each) do
+      sign_in(user)
+      visit product_path(product)
+    end
+
+    scenario 'user successfully adds review to product' do
+      fill_in 'Title', with: 'My Review'
+      fill_in 'Body', with: 'This doesn\'t fit!'
+      fill_in 'Product fit', with: 3
+      click_button 'Create Review'
+      expect(page).to have_content('Review successfully added')
+      expect(page).to have_content('My Review')
+    end
+
+    scenario 'user unsuccessfully adds review to product' do
+      fill_in 'Title', with: ''
+      fill_in 'Body', with: 'This doesn\'t fit!'
+      fill_in 'Product fit', with: 'asdf'
+      click_button 'Create Review'
+      expect(page).to have_content('Title can\'t be blank')
+      expect(page).to have_content('Product fit is not included in the list')
+    end
+  end
+
+
+  scenario 'Non-user tries to review product' do
     visit product_path(product)
     fill_in 'Title', with: 'My Review'
     fill_in 'Body', with: 'This doesn\'t fit!'
     fill_in 'Product fit', with: 3
     click_button 'Create Review'
-    expect(page).to have_content('Review successfully added')
-    expect(page).to have_content('My Review')
-  end
-
-  scenario 'user unsuccessfully adds review to product' do
-    sign_in(user)
-    visit product_path(product)
-    fill_in 'Title', with: ''
-    fill_in 'Body', with: 'This doesn\'t fit!'
-    fill_in 'Product fit', with: 'asdf'
-
-    click_button 'Create Review'
-    expect(page).to have_content('Title can\'t be blank')
-    expect(page).to have_content('Product fit is not included in the list')
+    expect(page).to have_no_content('Review successfully added')
+    expect(page).to have_content('You must be signed in')
   end
 end
