2a226c940782577b5e846290c10ecf3f1f1ee4de
justinhuynh <jhuynh@gmail.com>
2015-09-25 15:00:54 -0400

    wrote tests for votes

---
+++ spec/features/user_creates_vote_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+feature 'user votes on review', %{
+  As an authenticated user
+  I want to vote on a review
+  So that so that people can know whether the review is helpful
+
+  Acceptance Criteria:
+  - [ ] I must be logged in
+  - [ ] I must see that my vote is counted
+} do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:product) { FactoryGirl.create(:product_with_reviews) }
+
+  scenario 'user upvotes a review' do
+    sign_in(user)
+    visit product_path(product)
+    find('img.up1').click
+    save_and_open_page
+    # vote_count = find_by_id("vote_count_1").text.to_i
+    # expect(vote_count).to eq(213)
+    expect(product.reviews[0].thumbs_up).to eq(1)
+    expect(page).to have_content('Thanks for your vote')
+  end
+
+  scenario 'user downvotes a review' do
+    sign_in(user)
+    visit product_path(product)
+    find('img.down1').click
+    expect(product.reviews[0].thumbs_down).to eq(1)
+    expect(page).to have_content('Thanks for your vote')
+  end
+
+  scenario 'user is not signed in' do
+    visit product_path(product)
+    find('img.down1').click
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
+end
