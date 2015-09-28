281c9b0403c81dacc9024481eb8653e1e7a9e360
Eric Zell <EricJZell@gmail.com>
2015-09-28 14:20:52 -0400

    got rid of shoula user uniqueness test

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,11 +1,18 @@
 class Vote < ActiveRecord::Base
-  validates :review, presence: true
-  validates :user, presence: true
   belongs_to :review
   belongs_to :user
 
+  validates :review, presence: true
+  validates :user, presence: true
+
   validates :user, uniqueness: {
     scope: :review,
-    message: "only one vote per user"
+    message: "only one vote per user per review"
   }
+
+  validates :review, uniqueness: {
+    scope: :user,
+    message: "only one user per vote per review"
+  }
+
 end
