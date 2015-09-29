d56c922950337857cb208249c64fdc826f8b68da
Eric Zell <EricJZell@gmail.com>
2015-09-29 13:41:42 -0400

    waiting for ajax in capybara

--- spec/features/user_creates_vote_spec.rb
+++ spec/features/user_creates_vote_spec.rb
@@ -19,10 +19,10 @@ feature 'user votes on review', %{
     vote_count = find("li#review#{review_id} .up-count").text.to_i
     expect(vote_count).to eq(0)
     find("li#review#{review_id}").find(".up").click
-    save_screenshot("capybara-js.png")
+    sleep(0.5)
+    expect(page).to have_content('Thanks for your vote')
     vote_count = find("li#review#{review_id} .up-count").text.to_i
     expect(vote_count).to eq(1)
-    expect(page).to have_content('Thanks for your vote')
   end
 
   scenario 'user downvotes a review', js: true do
@@ -32,17 +32,17 @@ feature 'user votes on review', %{
     vote_count = find("li#review#{review_id} .down-count").text.to_i
     expect(vote_count).to eq(0)
     find("li#review#{review_id}").find(".down").click
-    save_screenshot("capybara-js.png")
+    sleep(0.5)
+    expect(page).to have_content('Thanks for your vote')
     vote_count = find("li#review#{review_id} .down-count").text.to_i
     expect(vote_count).to eq(1)
-    expect(page).to have_content('Thanks for your vote')
   end
 
   scenario 'user is not signed in', js: true do
     visit product_path(product)
     review_id = product.reviews[0].id
     find("li#review#{review_id}").find(".down").click
-    save_screenshot("capybara-js.png")
+    sleep(0.5)
     expect(page).to have_content('You must sign in to vote')
   end
 end
