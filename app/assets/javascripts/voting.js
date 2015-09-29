0c82525194ecfb830dcf6863d458845f11135901
Eric Zell <EricJZell@gmail.com>
2015-09-29 13:52:34 -0400

    fix hound errors

--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -1,8 +1,8 @@
 $(".vote").on("click", function(event) {
   event.preventDefault();
   var $this = $(this);
-  var reviewId = $this.data('reviewId');
-  var helpful = $this.data('helpful');
+  var reviewId = $this.data("reviewId");
+  var helpful = $this.data("helpful");
   var request = $.ajax({
     method: "POST",
     url: ('/reviews/' + reviewId + '/votes'),
@@ -11,16 +11,16 @@ $(".vote").on("click", function(event) {
   });
   request.success(function(data) {
     var review = $("#review"+reviewId);
-    var upcount = review.find('.up-count');
-    var downcount = review.find('.down-count');
-    var message = review.find('.ajax-message');
-    upcount.text(data['up']);
-    downcount.text(data['down']);
+    var upcount = review.find(".up-count");
+    var downcount = review.find(".down-count");
+    var message = review.find(".ajax-message");
+    upcount.text(data["up"]);
+    downcount.text(data["down"]);
     message.text("Thanks for your vote");
   });
   request.error(function() {
-    var review = $('#review'+reviewId);
-    var message = review.find('.ajax-message');
+    var review = $("#review"+reviewId);
+    var message = review.find(".ajax-message");
     message.text("You must sign in to vote");
   });
 });
