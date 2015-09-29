5f264770932134c29d4c0a2c63cf87667a1caca5
Sorachantha Chan <xwsgbx@gmail.com>
2015-09-29 13:53:17 -0400

    added tests for search v2

--- spec/features/user_searches_products_spec.rb
+++ spec/features/user_searches_products_spec.rb
@@ -10,20 +10,20 @@ feature 'user uses the search function', %{
   - [√] User results based on query
 
 } do
-    let!(:product) { FactoryGirl.create(:product_with_reviews) }
+  let!(:product) { FactoryGirl.create(:product_with_reviews) }
 
-    scenario "User succesffully uses the search function" do
-      visit products_path
-      fill_in "Search Products", with: product.title
-      click_button 'Search'
-      expect(page).to have_content(product.title)
-    end
-
-    scenario "User unsuccesffully uses the search function" do
-      visit products_path
-      fill_in "Search Products", with: 'something different'
-      click_button 'Search'
-      expect(page).to_not have_content(product.title)
-      expect(page).to have_content('There are no posts containing the term(s)')
-    end
+  scenario "User succesffully uses the search function" do
+    visit products_path
+    fill_in "Search Products", with: product.title
+    click_button 'Search'
+    expect(page).to have_content(product.title)
   end
+
+  scenario "User unsuccesffully uses the search function" do
+    visit products_path
+    fill_in "Search Products", with: 'something different'
+    click_button 'Search'
+    expect(page).to_not have_content(product.title)
+    expect(page).to have_content('There are no posts containing the term(s)')
+  end
+end
