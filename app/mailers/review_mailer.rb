d32a3cc31377af1e6edb26daeea29649ce8d46d0
Saurbier <saurbier@gmail.com>
2015-10-01 13:19:36 -0400

    hound error...

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,10 +1,9 @@
 class ReviewMailer < ApplicationMailer
   def new_review(review)
     @review = review
-
-     mail(
-       to: review.product.user.email,
-       subject: "New Review for #{review.product.title}"
-     )
+    mail(
+    to: review.product.user.email,
+    subject: "New Review for #{review.product.title}"
+    )
   end
 end
