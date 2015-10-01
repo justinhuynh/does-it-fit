272671ab05710f99f2c9de9323b29033b4d72fe5
Saurbier <saurbier@gmail.com>
2015-10-01 13:22:57 -0400

    hound

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -2,8 +2,8 @@ class ReviewMailer < ApplicationMailer
   def new_review(review)
     @review = review
     mail(
-    to: review.product.user.email,
-    subject: "New Review for #{review.product.title}"
+      to: review.product.user.email,
+      subject: "New Review for #{review.product.title}"
     )
   end
 end
