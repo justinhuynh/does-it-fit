44ecc3c62e6bb23aefe2c2dbed98fc9525773ebc
Sorachantha Chan <xwsgbx@gmail.com>
2015-10-01 16:33:43 -0400

    unloading to pull from master

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,4 +2,9 @@ class BrandsController < ApplicationController
   def index
     @brands = Brand.all
   end
+
+  def show
+    @brand = Brand.find(params[:id])
+    @products = @brand.products.page(params[:page]).per(10)
+  end
 end
