dc859651dab00197a73406cb6b98d8c236dfc0eb
Saurbier <saurbier@gmail.com>
2015-10-01 13:17:37 -0400

    hound error on production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_mailer.default_url_options = { host: "serene-oasis-8610.herokuapp.com" }
+  config.action_mailer.default_url_options = {
+    host: "serene-oasis-8610.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: "587",
