0b607e37f602918ef923639b911bbf0197935f5a
Saurbier <saurbier@gmail.com>
2015-10-02 11:41:06 -0400

    initial real seeds commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ User.create(email: 'EricJZell@gmail.com', password: '12345678')
   )
 end
 
-20.times do
-  Brand.create(name: Faker::Company.name)
-end
+# 20.times do
+#   Brand.create(name: Faker::Company.name)
+# end
 
 categories = [
   'Pants', 'Dress Shirts', 'Casual Shirts', 'Shoes', 'Jackets', 'Outerwear',
@@ -27,25 +27,48 @@ categories.each do |category|
   Category.create(name: category)
 end
 
-40.times do
+category = Category.find_by(name: 'Pants')
+brand = Brand.create(name: 'Patagonia')
+titles = [
+  'PATAGONIA MEN\'S VENGA ROCK PANTS',
+  'PATAGONIA MEN\'S REGULAR FIT JEANS',
+  'PATAGONIA MEN\'S STRAIGHT FIT CORDS'
+]
+urls = [
+  'http://www.patagonia.com/tsimages/83080_UWTB.fpx?wid=750&hei=750&bgcolor=FFFFFF&ftr=6&cvt=jpeg,scans=progressive',
+  'http://www.patagonia.com/tsimages/55940_DDNM.fpx?wid=750&hei=750&bgcolor=FFFFFF&ftr=6&cvt=jpeg,scans=progressive',
+  'http://www.patagonia.com/tsimages/55920_ASHT.fpx?wid=750&hei=750&bgcolor=FFFFFF&ftr=6&cvt=jpeg,scans=progressive'
+]
+
+count = 0
+titles.each do |title|
   Product.create(
-    title: Faker::Commerce.product_name,
-    brand: Brand.all.sample,
-    category: Category.all.sample,
-    average_fit: [*(1..10)].sample,
-    description: Faker::Lorem.sentences(5).join(' '),
-    user: User.all.sample
+    title: title,
+    brand: brand,
+    category: category,
+    user: User.all.sample,
+    image_url: urls[count]
   )
+  count +=1
 end
 
-Product.all.each do |product|
-  rand(10).times do
-    Review.create(
-      title: Faker::App.name,
-      body: Faker::Lorem.sentences(3).join(' '),
-      product_fit: [*(1..10)].sample,
-      user: User.all.sample,
-      product: product
-    )
-  end
-end
+# Product.all.each do |product|
+#   rand(10).times do
+#     Review.create(
+#       title: Faker::App.name,
+#       body: Faker::Lorem.sentences(3).join(' '),
+#       product_fit: [*(1..10)].sample,
+#       user: User.all.sample,
+#       product: product
+#     )
+#   end
+# end
+#
+#
+# Product.create(
+#   title: 'Wrinkle-Free Dress Chinos, Classic Fit Pleated',
+#   brand: Brand.all.sample,
+#   category: Category.all.sample,
+#   average_fit: [*(1..10)].sample,
+#   description: Faker::Lorem.sentences(5).join(' '),
+#   user: User.all.sample
